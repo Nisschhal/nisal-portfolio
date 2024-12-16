b77f9207a30e273ea02ecf353c55873db2b1b96f
nischal puri <mrnischalpuri@gmail.com>
2024-12-16 12:34:12 +0545

    custom card added for reusable component'

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Card from "@/components/ui/card"
 import WaterWaveWrapper from "@/components/visual-effects/water-wave"
 import Image from "next/image"
 import Link from "next/link"
@@ -7,11 +8,18 @@ export default function Home() {
   return (
     <WaterWaveWrapper
       imageUrl=""
-      dropRadius="5"
+      dropRadius="3"
       pertrubuance="3"
       resolution="2048"
     >
-      {() => <div className="h-screen"></div>}
+      {() => (
+        <div className="h-screen p-20">
+          {/* Custom Card: title, classNames, and children */}
+          <Card classNames="text-white max-w-2xl mx-auto" title="Card Title">
+            ...
+          </Card>
+        </div>
+      )}
     </WaterWaveWrapper>
   )
 }
