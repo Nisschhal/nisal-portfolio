84a286bcfa9673c7ce67566037bdee6d4786627b
nischal puri <mrnischalpuri@gmail.com>
2024-12-30 10:26:11 +0545

    menu item added with animation

--- components/navigation/full-screen-menu/fullScreenMenu.tsx
+++ components/navigation/full-screen-menu/fullScreenMenu.tsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion"
 import { menuSlide } from "./animation"
 import Curve from "./curve"
+import Profile from "@/components/ui/profile"
+import NavLink from "./navLink"
+import { AnimatePresence } from "framer-motion"
 
 export default function FullScreenMenu() {
   return (
@@ -11,8 +14,30 @@ export default function FullScreenMenu() {
       exit="exit"
       className="h-screen w-full bg-black fixed top-0 right-0 text-primary-foreground z-40 font-oswald "
     >
+      {/* Profile  */}
+      <div className="relative w-full max-w-[95%] mx-auto">
+        <div className="absolute top-8">
+          <Profile />
+        </div>
+      </div>
+      {/* Menu card */}
+      <div className="absolute bottom-32 w-full lg:pl-[5%]">
+        <div
+          className="grid relative"
+          style={{
+            gridTemplateColumns: "1fr 500px",
+          }}
+        >
+          <div className="pl-4 flex flex-col justify-end">
+            <AnimatePresence>
+              {navItems.map((navItem, index) => (
+                <NavLink key={index} item={{ ...navItem, index }} />
+              ))}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
       {/*Curve svg effect*/}
-
       <Curve />
     </motion.div>
   )
---
+++ components/navigation/full-screen-menu/navLink.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion"
+import { scale, slide } from "./animation"
+import Link from "next/link"
+
+interface NavLinkProps {
+  item: { title: string; href: string; index: number }
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ item }) => {
+  const { title, href, index } = item
+  return (
+    <motion.div
+      className="relative flex items-center"
+      variants={slide}
+      custom={index}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+    >
+      <motion.div variants={scale} className="hidden"></motion.div>
+      <Link href={href} className="text-[6vw] uppercase leading-[96%]">
+        {title}
+      </Link>
+    </motion.div>
+  )
+}
+
+export default NavLink
