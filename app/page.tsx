10d660d2ccecee29e89678eec2b213bc7cf7894a
nischal puri <mrnischalpuri@gmail.com>
2025-01-21 14:07:28 +0545

    made water wave dynami for client

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 "use client"
-import WaterWaveWrapper from "@/components/visual-effects/water-wave"
+// import WaterWaveWrapper from "@/components/visual-effects/water-wave"
 import AboutSection from "@/sections/aboutSection"
 import ContactSetion from "@/sections/contactSection"
 import FeaturedSection from "@/sections/featuredSection"
 import LandingSection from "@/sections/landingPage"
 import { Toaster } from "sonner"
 import gsap from "gsap"
+import dynamic from "next/dynamic"
+
+// Dynamic import WaterWaveWrapper to ensure it's only used in the client side environment
+const WaterWaveWrapper = dynamic(
+  () => import("@/components/visual-effects/water-wave"),
+  { ssr: false }
+)
+
 export default function Home() {
   const tl = gsap.timeline({
     defaults: { duration: 0.75, ease: "Power3.easeOut" },
   })
 
-  // useGSAPAnimation()
   return (
+    // its only on the client side
     <WaterWaveWrapper
       imageUrl=""
       dropRadius="3"
